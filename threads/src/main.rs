3cc9b2bd3e3d78d83ea35fca97572a181041dbaa
Jarek <jarek.dziedzic@gmail.com>
2022-01-12 18:50:01 +0100

    Extend threads example with multi producer channels and atomic reference counting pointers

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     threads1();
     threads2();
     threads3();
+    threads4();
+    threads5();
 }
 
 fn threads1() {
@@ -36,9 +38,54 @@ fn threads3() {
     std::thread::spawn(move || {
         let val = String::from("Hello!");
         tx.send(val).expect("Failed to send to a channel");
-        println!("val is {}", val);
     });
 
     let result = rx.recv();
     println!("Result: {:?}", result);
+}
+
+fn threads4(){
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    let tx1 = tx.clone();
+    std::thread::spawn(move || {
+        for elem in vec!["Item 1", "Item 2", "Item 3"] {
+            tx1.send(elem).expect("Failed to send from tx1");
+        }
+    });
+
+    let tx2 = tx.clone();
+    std::thread::spawn(move || {
+        for elem in vec!["Item 4", "Item 5", "Item 6"] {
+            tx2.send(elem).expect("Failed to send from tx2");
+        }
+    });
+
+    for _ in 1..6 {
+        println!("Received {}", rx.recv().unwrap());
+    }
+}
+
+fn threads5() {
+    use std::sync::{Arc,Mutex};
+    use std::thread;
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let cloned_counter = counter.clone();
+        let handle = thread::spawn(move || {
+            let mut num = cloned_counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
